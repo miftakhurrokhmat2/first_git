f8658ab0769f2daf19c7620674f422b55b72195e
miftakhurrokhmat2 <miftakhurrokhmat@alumni.uii.ac.id>
2024-05-06 12:42:56 +0700

    Update Nim18917214.java
    tambah gelar

--- Nim18917214.java
+++ Nim18917214.java
@@ -4,6 +4,6 @@
  */
 public class Nim18917214 {
     public static void main(String[] args) {
-        System.out.println("Miftakhurokhmat");
+        System.out.println("Miftakhurokhmat, S.Kom.");
     }
 }
