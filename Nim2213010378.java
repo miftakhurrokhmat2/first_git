0a6d01a3469ba453fcdcfcfa43ab813d208bc876
IlharshFerocity <161426872+IlharshFerocity@users.noreply.github.com>
2024-05-05 22:22:07 -0700

    Add files via upload

---
+++ Nim2213010378.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author Amikom Solo
+ */
+public class Nim2213010378 {
+    public static void main(String[] args) {
+        System.out.println("Ilham Pebrianto Eko Saputra");
+    }
+}
